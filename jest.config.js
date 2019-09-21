5b081f5a360bc96a3333b4e3bb559716b485e100
Khieu Le <khieulq@gmail.com>
2019-09-21 23:37:37 +0700

    improve config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-commonjs */
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
@@ -58,7 +59,11 @@ module.exports = {
     // globalTeardown: null,
 
     // A set of global variables that need to be available in all test environments
-    // globals: {},
+    globals: {
+        'ts-jest': {
+            diagnostics: false,
+        },
+    },
 
     // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
     // maxWorkers: "50%",
