8aa10dd31adf6914d75c47d2a2e365a38cafb01e
Khieu Le <khieulq@gmail.com>
2019-09-22 11:04:19 +0700

    refactor sock-merchant

---
+++ src/global/dictionary.d.ts
@@ -0,0 +1,1 @@
+type Dictionary<K extends keyof any, T> = { [P in K]?: T };
--- src/sock-merchant.ts
+++ src/sock-merchant.ts
@@ -1,17 +1,18 @@
 type Sock = number;
-interface SockPair {
-    [color: number]: Sock[],
-}
+
 const countPair = (socks: Sock[]) => Math.floor(socks.length / 2);
+
 export default function sockMerchant(n: number, ar: number[]) {
     if (n !== ar.length) throw new Error('number of socks does not match!');
-    const sockPair: SockPair = {};
+    const sockPair: Dictionary<number, Sock[]> = {};
     ar.forEach(sock => {
         const socksWithSameColor = sockPair[sock] || <Sock[]>[];
         sockPair[sock] = [...socksWithSameColor, sock];
     });
     const numberOfPair = Object.entries(sockPair)
-        .map(([, socks]) => countPair(socks))
+        .map(([, socks]) => countPair(socks as Sock[]))
         .reduce((a, b) => a + b, 0);
     return numberOfPair;
 }
+
+sockMerchant(1, [1]);
