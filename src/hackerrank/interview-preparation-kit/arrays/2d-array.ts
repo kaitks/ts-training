414521e9e212cff7376da0c1028b183ad935f8e8
Khieu Le <khieulq@gmail.com>
2019-09-23 20:21:35 +0700

    do 2d-array

---
+++ src/hackerrank/interview-preparation-kit/arrays/2d-array.ts
@@ -0,0 +1,32 @@
+type TwoDimensionArray = number[][];
+type Hourglass = [[number, number, number], [number], [number, number, number]];
+const GLASS_SIZE = 3;
+
+const sumHourglass = (hourglass: Hourglass): number => {
+    return hourglass.flat(2).reduce((a: number, b: number) => a + b, 0);
+};
+
+export default function hourglassSum(arr: TwoDimensionArray) {
+    const arrLength = arr.length;
+    let x = 0;
+    let y = 0;
+    const hourglasses: Hourglass[] = [];
+
+    const limit = arrLength - GLASS_SIZE;
+    while (true) {
+        const hourglass: Hourglass = [
+            [arr[y][x], arr[y][x + 1], arr[y][x + 2]],
+            [arr[y + 1][x + 1]],
+            [arr[y + 2][x], arr[y + 2][x + 1], arr[y + 2][x + 2]],
+        ];
+        hourglasses.push(hourglass);
+        if (x === limit && y === limit) break;
+        x++;
+        if (x === limit && y < limit) {
+            x = 0;
+            y++;
+        }
+    }
+    const hourglassSumList = hourglasses.map(sumHourglass);
+    return Math.max(...hourglassSumList);
+}
---
+++ src/hackerrank/interview-preparation-kit/arrays/__tests__/2d-array.test.ts
@@ -0,0 +1,13 @@
+import hourglassSum from "../2d-array";
+
+test("hourglassSum should pass", () => {
+    const result = hourglassSum([
+        [1, 1, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0],
+        [0, 0, 2, 4, 4, 0],
+        [0, 0, 0, 2, 0, 0],
+        [0, 0, 1, 2, 4, 0],
+    ]);
+    expect(result).toBe(19);
+});
