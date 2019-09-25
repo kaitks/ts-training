d4c8a2f949dec1361063cec7b4d80c6984dea582
lkhieu <lkhieu@yahoo-corp.jp>
2019-09-25 10:55:08 +0700

    restructure

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import sockMerchant from "./hackerrank/interview-preparation-kit/warmup/solution/sock-merchant";
-import jumpingOnClouds from "./hackerrank/interview-preparation-kit/warmup/solution/jumping-on-clouds";
-import repeatedString from "./hackerrank/interview-preparation-kit/warmup/solution/repeated-string";
-import countingValleys from "./hackerrank/interview-preparation-kit/warmup/solution/counting-valleys";
+import sockMerchant from "./hackerrank/warmup/solution/sock-merchant";
+import jumpingOnClouds from "./hackerrank/warmup/solution/jumping-on-clouds";
+import repeatedString from "./hackerrank/warmup/solution/repeated-string";
+import countingValleys from "./hackerrank/warmup/solution/counting-valleys";
 
 export { sockMerchant, jumpingOnClouds, repeatedString, countingValleys };
