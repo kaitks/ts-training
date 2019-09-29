f0b9e64bfafcf9b63754bd7783df6ee1c8ef5993
Khieu Le <khieulq@gmail.com>
2019-09-29 11:33:39 +0700

    finish 10-days button grid

--- src/hackerrank/10-days-js/solution/button-container/js/buttonsGrid.js
+++ src/hackerrank/10-days-js/solution/button-container/js/buttonsGrid.js
@@ -1,53 +1,27 @@
-const buttons = Object.entries({
-    btn1: 1,
-    btn2: 2,
-    btn3: 3,
-    btn6: 6,
-    btn9: 9,
-    btn8: 8,
-    btn7: 7,
-    btn4: 4,
-});
+const buttonLabelList = [1, 2, 3, 6, 9, 8, 7, 4];
 
-const container = document.querySelector("#btns");
+const mapToDisplayOrder = buttonLabelList => [
+    buttonLabelList[0],
+    buttonLabelList[1],
+    buttonLabelList[2],
+    buttonLabelList[7],
+    buttonLabelList[3],
+    buttonLabelList[6],
+    buttonLabelList[5],
+    buttonLabelList[4],
+];
 
-const drawButtons = buttons => {
-    const buttonElementList = buttons.map(([k, v]) => {
-        const element = document.createElement("button");
-        element.id = k;
-        element.innerHTML = v;
-        return element;
-    });
-
-    return buttonElementList;
-};
-
-const rotateButtons = buttons => {
+const rotateButtonLabel = buttons => {
     const last = buttons.pop();
     buttons.unshift(last);
     return buttons;
 };
 
-const drawTable = buttons => {
-    container.innerHTML = "";
-    const orderedButtons = [
-        buttons[0],
-        buttons[1],
-        buttons[2],
-        buttons[7],
-        buttons[3],
-        buttons[6],
-        buttons[5],
-        buttons[4],
-    ];
-    orderedButtons.splice(4, 0, ["btn5", 5]);
-    const buttonElementList = drawButtons(orderedButtons);
-    buttonElementList.forEach(element => container.appendChild(element));
-
-    document.getElementById("btn5").onclick = () => {
-        const rotatedButtons = rotateButtons(buttons);
-        drawTable(rotatedButtons);
-    };
+document.getElementById("btn5").onclick = () => {
+    const rotatedButtons = rotateButtonLabel(buttonLabelList);
+    const orderedButtons = mapToDisplayOrder(rotatedButtons);
+    [1, 2, 3, 4, 6, 7, 8, 9].forEach((id, index) => {
+        if (id === 5) return;
+        document.getElementById(`btn${id}`).innerHTML = orderedButtons[index];
+    });
 };
-
-drawTable(buttons);
