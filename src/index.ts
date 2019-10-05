f50c13c3bb77e17208ccc03c3fb0a5251e79248d
Khieu Le <khieulq@gmail.com>
2019-10-05 20:04:25 +0700

    remove useless index

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import sockMerchant from "./hackerrank/warmup/solution/sock-merchant";
-import jumpingOnClouds from "./hackerrank/warmup/solution/jumping-on-clouds";
-import repeatedString from "./hackerrank/warmup/solution/repeated-string";
-import countingValleys from "./hackerrank/warmup/solution/counting-valleys";
-
-export { sockMerchant, jumpingOnClouds, repeatedString, countingValleys };
