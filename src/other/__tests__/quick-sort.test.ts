ef69283488526d5ad1b4b3dd0cdc681ca041d352
Khieu Le <khieulq@gmail.com>
2019-10-06 12:28:21 +0700

    finish quick-sort median

--- src/other/__tests__/quick-sort.test.ts
+++ src/other/__tests__/quick-sort.test.ts
@@ -1,14 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import faker from "faker";
-import quickSort from "../quick-sort";
+import quickSort from "../quick-sort-first";
 
-test("", () => {
+test("random input", () => {
     const input = Array.from(Array(9).keys()).map(() =>
         faker.random.number({
             min: 1,
             max: 99,
         })
     );
+    const result = quickSort(input, 0, input.length - 1);
+    console.log(result);
+});
+
+test("custom input 1", () => {
+    const input = [43, 13, 43, 22, 70, 62, 76, 2, 7];
+    const result = quickSort(input, 0, input.length - 1);
+    console.log(result);
+});
+
+test("custom input 2", () => {
+    const input = [26, 40, 79, 53, 72, 79, 95, 92, 94];
     const result = quickSort(input, 0, input.length - 1);
     console.log(result);
 });
---
+++ src/other/quick-sort-median.ts
@@ -0,0 +1,30 @@
+import arraySwap from "../lib/array-swap";
+
+export default function quickSort(arr: number[], start: number, end: number) {
+    if (start >= end) return arr;
+    let pivot = Math.floor((start + end) / 2);
+    const pivotValue = arr[pivot];
+    let i = start;
+    let j = end;
+    while (i < j) {
+        while (arr[i] < pivotValue) {
+            i++;
+        }
+        while (arr[j] > pivotValue) {
+            j--;
+        }
+        if (arr[i] === pivotValue && arr[j] === pivotValue) {
+            j--;
+        } else {
+            arraySwap(arr, i, j);
+            if (i === pivot) {
+                pivot = j;
+            } else if (j === pivot) {
+                pivot = i;
+            }
+        }
+    }
+    quickSort(arr, start, pivot - 1);
+    quickSort(arr, pivot + 1, end);
+    return arr;
+}
